47ece664cb82bd1f10c7fd9b1bd6f4ec7acca1a8
RichardWinterhalder <richard.winterhalder@rubicon.eu>
2023-03-24 12:46:42 +0100

    -init view

---
+++ frontend/elevator-dashboard/src/components/ElevatorStatus.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { ElevatorStatusElement } from '@/domain';
+
+defineProps<{
+  elements: ElevatorStatusElement[]
+}>()
+</script>
+
+<template>
+     <div class="container mt-2">
+      <ul class="list-group">
+        <li v-for="element in elements" :key="element.id" class="list-group-item">Elev{{element.id }}, {{ element.author }}</li>
+      </ul>
+  </div>
+</template>
